d5194dd8291e8ac7b7778f334768c2b2dcd9abb4
Odissey <71000942+odyssey-hub@users.noreply.github.com>
2024-04-29 12:37:32 +0300

    Update Bot Token

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import telebot
 
 from chatbot.db.SQLite.DB import DB
 
-bot = telebot.TeleBot('6346571492:AAFF2VAFvfok54GeZNCO98ET8uTxapKvZ6s')
+bot = telebot.TeleBot('TOKEN')
 project_dir = os.path.dirname(__file__)
 db = DB(os.path.join(project_dir, 'BotDB.db'))
